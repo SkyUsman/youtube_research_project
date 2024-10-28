0e65b65f05d2cdc6e053df6455ed58fbb182b42f
Rajie04 <rtahmed04@gmail.com>
2024-10-27 23:37:50 -0500

    update cors for vercel link

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ import re
 from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["https://youtube-research-project.vercel.app/", "http://localhost:3000"])  # Allow only this origin
+CORS(app, origins=["https://youtube-research-project.vercel.app", "http://localhost:3000"]) # Allow these two origins
 
 # Database connection parameters
 server = 'youtube-comments.database.windows.net'
